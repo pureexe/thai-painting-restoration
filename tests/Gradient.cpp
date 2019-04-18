c7bca216884f42848e4eae3e3d6128b74a1d1d6e
Pakkapon Phongthawee <me@pureapp.in.th>
2019-04-18 18:29:02 +0700

    fix: true

--- tests/Gradient.cpp
+++ tests/Gradient.cpp
@@ -12,7 +12,7 @@ int main()
 {
     Mat origial_01 = (Mat_<double>(3,3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
     Mat expected_01_01 =  (Mat_<double>(3,6) << -1, 6, -1, 0, 0, 0 , 1 , -6, 1, -1, 0, 1, 6, 0, -6, -1, 0, 1);
-    bool TEST_PASS = TRUE;
+    bool TEST_PASS = true;
     TEST_PASS = TEST_PASS && equal(Gradient(origial_01),expected_01_01);
     if(TEST_PASS){
         return 0;
