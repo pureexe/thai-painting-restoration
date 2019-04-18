bdbbf8420e7cd12a074cddc547cdc5452968b887
Pakkapon Phongthawee <me@pureapp.in.th>
2019-04-18 14:52:56 +0700

    add: create lambda metric

---
+++ src/inpainter/CreateLambdaMatrix.h
@@ -0,0 +1,7 @@
+#include "toDoubleBand.h"
+Mat CreateLambdaMatrix(Mat inpaint_domain,double lambda)
+{
+	Mat inpainted_lambda = (inpaint_domain == 0);
+	inpainted_lambda = toDoubleBand(inpainted_lambda);
+	return inpainted_lambda.mul(lambda);
+}
