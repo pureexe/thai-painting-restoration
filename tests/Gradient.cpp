5c7172df0ac2cd9bc80a286e30fb519c64b5c139
Pakkapon Phongthawee <me@pureapp.in.th>
2019-04-18 18:35:29 +0700

    fix: test case error

--- tests/Gradient.cpp
+++ tests/Gradient.cpp
@@ -11,7 +11,7 @@ using namespace cv;
 int main()
 {
     Mat origial_01 = (Mat_<double>(3,3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
-    Mat expected_01_01 =  (Mat_<double>(3,6) << -1, 6, -1, 0, 0, 0 , 1 , -6, 1, -1, 0, 1, 6, 0, -6, -1, 0, 1);
+    Mat expected_01_01 =  (Mat_<double>(6,3) << -1, 6, -1, 0, 0, 0 , 1 , -6, 1, -1, 0, 1, 6, 0, -6, -1, 0, 1);
     bool TEST_PASS = true;
     TEST_PASS = TEST_PASS && equal(Gradient(origial_01),expected_01_01);
     if(TEST_PASS){
