9aa6ed6f8261aff8675b72d56fb3d4c4a2b054bf
Pakkapon Phongthawee <me@pureapp.in.th>
2019-04-18 20:44:44 +0700

    no longer support travis-ci

--- src/header.h
+++ src/header.h
@@ -3,10 +3,10 @@
 
 // TODO: add headers that you want to pre-compile here
 
-#include "opencv2/core.hpp"
-#include "opencv2/photo.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/photo.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 
